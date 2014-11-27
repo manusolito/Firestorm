62c3f786d4ffd33b2446541325e713e8aaf2d5ae
nahuel071 <nahuel.ast@gmail.com>
2014-11-27 12:53:20 -0300

    elegir ganador incompleto
    no se como hacer que se "active " el metodo ganador pasandole como
    parametro el id de oferta.

--- Bestnid/app/controllers/productos_controller.rb
+++ Bestnid/app/controllers/productos_controller.rb
@@ -51,8 +51,14 @@ class ProductosController < ApplicationController
            flash[:success] = "Producto Eliminado"
            redirect_to producto_url
     end
+  def ganador
+    @producto.ofertagano_id = params[:ofertagano_id]
+    @producto.save
+    flash[:success] = "Ganador elegido"
+
+  end
   private
     def producto_params
-      params.require(:productos).permit(:nombre, :descripcion, :vencimiento, :prourl, :categoria_id )
+      params.require(:productos).permit(:nombre, :descripcion, :vencimiento, :prourl, :categoria_id, :ofertagano_id )
     end
 end
